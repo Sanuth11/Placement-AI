72e61c6c1d916b3b05667af2be38252d3bdb400c
Sanuth11 <sanuthh@gmail.com>
2026-04-30 21:43:04 +0530

    fix new cors error

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,7 @@ const historyRoutes = require("./routes/history");
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(require("cors")());
 
 app.use(express.json());
 app.use("/api/resume", resumeRoutes);
